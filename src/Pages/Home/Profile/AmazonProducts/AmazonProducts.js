29ac883793c14fa318a8b6f7b36edc20b62e96a7
sanaul01 <mdsanaulislam154@gmail.com>
2022-03-14 22:55:30 +0600

    amazon product uploaded

--- src/Pages/Home/Profile/AmazonProducts/AmazonProducts.js
+++ src/Pages/Home/Profile/AmazonProducts/AmazonProducts.js
@@ -1,8 +1,18 @@
-import { Box, Card, CardActions, CardContent, CardMedia, Grid, Typography } from '@mui/material';
-import React from 'react';
+import { Box, Button, Card, CardActions, CardContent, CardMedia, Grid, Typography } from '@mui/material';
+import React, { useEffect, useState } from 'react';
 import ProfileHeader from "../ProfileHeader/ProfileHeader";
 
 const AmazonProducts = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/products`)
+        .then(res => res.json())
+        .then(data=> setProducts(data?.products))
+    }, [])
+
+    console.log(products)
+   
     return (
         <Box sx={{ flexGrow: 1, paddingLeft: "72px" }} >
             <ProfileHeader />
@@ -10,20 +20,21 @@ const AmazonProducts = () => {
                 <Typography>Use CODE: proPlayer23 for 10% Discount</Typography>
             </Box>
             <Grid container spacing={{ xs: 2, md: 2 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-                {Array.from(Array(12)).map((_, index) => (
-                    <Grid item xs={2} sm={4} md={2} key={index}>
+                {products?.map((product) => (
+                    <Grid item xs={2} sm={4} md={2} key={product._id}>
                         <Card 
                         sx={{ 
                             maxWidth: 345, 
                             backgroundColor: 'rgba(0, 0, 0, 0.2)', 
                             olor: 'rgba(255, 255, 255, 0.809)', 
                             }}>
-                            <CardMedia
+                                <img src={`data:image/jpeg;base64,${product.imageBuffer}`} alt="" />
+                            {/* <CardMedia
                                 component="img"
                                 width="100%"
-                                image="https://upload.wikimedia.org/wikipedia/commons/9/9a/Gull_portrait_ca_usa.jpg"
+                                image={`data:image/jpeg;base64,${product.image}`}
                                 alt="green iguana"
-                            />
+                            /> */}
                             <CardContent>
                                 <Typography 
                                 style={{ 
@@ -32,7 +43,7 @@ const AmazonProducts = () => {
                                     }} 
                                     component="div"
                                     >
-                                    Lorem ipsum dolor
+                                    {product.ProductTitle}
                                 </Typography>
                                 <Typography 
                                 style={{
@@ -41,14 +52,15 @@ const AmazonProducts = () => {
                                      }} 
                                      component="div"
                                      >
-                                    Lizard
+                                    {product.productTitle}
                                 </Typography>
                                 <Typography variant="body2" color="text.secondary" style={{
                                      fontWeight: 400, 
                                      color: 'rgba(255, 255, 255, 0.809)' 
                                      }}>
-                                    $3323.00
+                                    ${product.productPrice}
                                 </Typography>
+                                <Button>Buy Now</Button>
                             </CardContent>
 
                         </Card>
