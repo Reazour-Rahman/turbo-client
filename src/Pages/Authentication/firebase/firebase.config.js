b6178c0f11fbfb228eac8b445ff751c271cf46f5
Reazour Rahaman Rabbi <alexrabbi111@gmail.com>
2022-03-16 00:38:53 +0600

    hh

--- src/Pages/Authentication/firebase/firebase.config.js
+++ src/Pages/Authentication/firebase/firebase.config.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyDgriXqSp7GfCioUViUHvN_QOztaQG-2cc",
-  authDomain: "proplayers-fb9c0.firebaseapp.com",
-  projectId: "proplayers-fb9c0",
-  storageBucket: "proplayers-fb9c0.appspot.com",
-  messagingSenderId: "866373135902",
-  appId: "1:866373135902:web:0e5327cdbd17abd08e080f"
+  apiKey: "AIzaSyD-czdmWW0zmWS8YQ5-pKfjKBeHw9LGKww",
+  authDomain: "turbo-dynamics-pro.firebaseapp.com",
+  projectId: "turbo-dynamics-pro",
+  storageBucket: "turbo-dynamics-pro.appspot.com",
+  messagingSenderId: "472959875726",
+  appId: "1:472959875726:web:b3ba56785e0235f4f667df"
 };
 export default firebaseConfig;
