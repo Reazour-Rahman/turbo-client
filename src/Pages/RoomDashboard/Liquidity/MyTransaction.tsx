b14c2a5823478061fbb5074804520c98de60c57e
unknown <apuemdad96@gmail.com>
2022-03-15 21:56:14 +0600

    user gets transaction

---
+++ src/Pages/RoomDashboard/Liquidity/MyTransaction.tsx
@@ -0,0 +1,103 @@
+import React, { useEffect, useState } from "react";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Fade from "@mui/material/Fade";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import { Avatar, Divider, IconButton } from "@mui/material";
+import { deepOrange, deepPurple } from "@mui/material/colors";
+
+const MyTransaction = ({ user }) => {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const [users, setUsers] = useState([]);
+  const payments = [];
+
+  const url = `http://localhost:5000/user/${user.email}`;
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setUsers(data);
+      });
+  }, []);
+
+  // console.log(payments);
+
+  //   console.log(payments);
+  console.log(users?.payment);
+
+  return (
+    <div className="members-container">
+      <div className="new-members-head">
+        <p>Latest Transactions</p>
+        <div>
+          <IconButton
+            className="add-btn"
+            id="fade-button"
+            aria-controls={open ? "fade-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+            onClick={handleClick}
+          >
+            <MoreHorizIcon />
+          </IconButton>
+
+          <Menu
+            id="fade-menu"
+            MenuListProps={{
+              "aria-labelledby": "fade-button",
+            }}
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            TransitionComponent={Fade}
+          >
+            <MenuItem onClick={handleClose}>Profile</MenuItem>
+            <MenuItem onClick={handleClose}>My account</MenuItem>
+            <MenuItem onClick={handleClose}>Logout</MenuItem>
+          </Menu>
+        </div>
+      </div>
+      <br />
+      <table id="customers">
+        <tr>
+          <th>Doner</th>
+          <th>Date</th>
+          <th>Time</th>
+          <th>Amount</th>
+          <th>Blog </th>
+          <th>Status</th>
+        </tr>
+
+        {users.payment.map((payment) => (
+          <tr>
+            <td className="td-flx">
+              {/* <Avatar
+                sx={{ bgcolor: deepPurple[500] }}
+                src={payment.bloggerPhoto}
+              ></Avatar> */}
+              {payment.doner}
+            </td>
+            <td>{payment.time}</td>
+            <td>{payment.date}</td>
+            <td>$ {payment.amount / 100}</td>
+            <td>{payment.blogTitle}</td>
+            <td className="status-transaction">Withdraw</td>
+          </tr>
+        ))}
+      </table>
+    </div>
+  );
+};
+
+export default MyTransaction;
---
+++ src/Pages/RoomDashboard/Liquidity/NewSubscriber.tsx
@@ -0,0 +1,97 @@
+import React, { useEffect, useState } from "react";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Fade from "@mui/material/Fade";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import { IconButton } from "@mui/material";
+import Avatar from "@mui/material/Avatar";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+
+const NewSubscriber = ({ user }) => {
+  const avatar = user.photoURL;
+
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://aqueous-chamber-45567.herokuapp.com/users")
+      .then((response) => response.json())
+      .then((data) => setUsers(data));
+  }, []);
+  console.log(users);
+
+  return (
+    <div className=" members-container">
+      <section>
+        <div className="new-members-head">
+          <p>My Subscribers</p>
+          <div>
+            <IconButton
+              className="add-btn"
+              id="fade-button"
+              aria-controls={open ? "fade-menu" : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? "true" : undefined}
+              onClick={handleClick}
+            >
+              <MoreHorizIcon />
+            </IconButton>
+
+            <Menu
+              id="fade-menu"
+              MenuListProps={{
+                "aria-labelledby": "fade-button",
+              }}
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              TransitionComponent={Fade}
+            >
+              <MenuItem onClick={handleClose}>Profile</MenuItem>
+              <MenuItem onClick={handleClose}>My account</MenuItem>
+              <MenuItem onClick={handleClose}>Logout</MenuItem>
+            </Menu>
+          </div>
+        </div>
+        <br />
+        <div>
+          {users.map((user) => (
+            <section className="team-members-list">
+              <aside className="team-members-id">
+                <div>
+                  <Avatar alt="Remy Sharp" src={avatar} />
+                </div>
+                <div>
+                  <p className="paragraph-size ">{user.name}</p>
+                  <p className=" p-dark">{user.email}</p>
+                </div>
+              </aside>
+
+              <div>
+                <Button
+                  className="add-btn"
+                  variant="outlined"
+                  size="small"
+                  startIcon={<PersonAddIcon />}
+                >
+                  Add
+                </Button>
+              </div>
+            </section>
+          ))}
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default NewSubscriber;
