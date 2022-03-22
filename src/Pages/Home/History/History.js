d137fcac6fca2c0e197d43266e77c711a2dc853a
unknown <apuemdad96@gmail.com>
2022-03-22 23:56:15 +0600

    history pagination half done

--- src/Pages/Home/History/History.js
+++ src/Pages/Home/History/History.js
@@ -1,40 +1,64 @@
-import { Grid } from '@mui/material';
-import React, { useState } from 'react';
-import './History.css';
-import HistoryList from './HistoryList';
-import HistoryRight from './HistoryRight';
-import HistoryVideo from './HistoryVideo'
-
+import { Grid } from "@mui/material";
+import React, { useState } from "react";
+import LoadMore from "../BLogs/LoadMore";
+import "./History.css";
+import HistoryList from "./HistoryList";
+import HistoryRight from "./HistoryRight";
+import HistoryVideo from "./HistoryVideo";
 
 const History = () => {
+  const [page, setPage] = useState(0);
+  const [loadMore, setLoadMore] = useState(true);
 
-    let theme;
-    theme = localStorage.getItem("theme");
-    let text = theme === "light" ? "black" : "darkLight";
-    return (
-        <Grid container columns={{ xs: 12, sm: 12, md: 12, lg: 12 }} className="history-container">
-            <Grid item xs={8} sm={8} md={9} lg={8} id={theme === "light" ? "moreLight" : "moreDark"}>
-                <div className='history-friction-element'>
-                    <h5 id={text}>Watch history</h5>
-                    <br />
-                    <h5 id={text}>All time</h5>
-                    <br />
-                    <section >
-                        {/* :::::::::::::::::::::::::::::::
+  let theme;
+  theme = localStorage.getItem("theme");
+  let text = theme === "light" ? "black" : "darkLight";
+
+  const handleLoadMore = () => {
+    setPage(page + 1);
+    // setLoadMore(true);
+    console.log("load more");
+  };
+  return (
+    <Grid
+      container
+      columns={{ xs: 12, sm: 12, md: 12, lg: 12 }}
+      className="history-container"
+    >
+      <Grid
+        item
+        xs={8}
+        sm={8}
+        md={9}
+        lg={8}
+        id={theme === "light" ? "moreLight" : "moreDark"}
+      >
+        <div className="history-friction-element">
+          <h5 id={text}>Watch history</h5>
+          <br />
+          <h5 id={text}>All time</h5>
+          <br />
+          <section>
+            {/* :::::::::::::::::::::::::::::::
                     Please Map here for Video dynamic
                     :::::::::::::::::::::::::::::::::*/}
-                        <HistoryList />
-                    </section>
-
-                </div>
-            </Grid>
-            <Grid item xs={4} sm={4} md={3} lg={4}>
-                <div className='history-friction-element-2'>
-                    <HistoryRight />
-                </div>
-            </Grid>
-        </Grid>
-    );
+            <HistoryList page={page} />
+          </section>
+          <div
+            onClick={() => handleLoadMore()}
+            style={{ display: "flex", justifyContent: "center" }}
+          >
+            {loadMore && <LoadMore />}
+          </div>
+        </div>
+      </Grid>
+      <Grid item xs={4} sm={4} md={3} lg={4}>
+        <div className="history-friction-element-2">
+          <HistoryRight />
+        </div>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default History;
--- src/Pages/Home/History/HistoryList.js
+++ src/Pages/Home/History/HistoryList.js
@@ -4,18 +4,17 @@ import { useSelector } from "react-redux";
 import History from "./History";
 import HistoryVideo from "./HistoryVideo";
 
-const HistoryList = () => {
-
-
+const HistoryList = ({ page }) => {
   const [histories, setHistories] = useState([]);
   const [searchHistories, setsearchHistories] = useState([]);
   const [loading, setLoading] = useState(false);
-  const user = useSelector(state => state.firebase.user)
+  const user = useSelector((state) => state.firebase.user);
+  const size = 5;
   useEffect(() => {
     setLoading(true);
     setTimeout(async () => {
       const response = await fetch(
-        "https://aqueous-chamber-45567.herokuapp.com/views"
+        `http://localhost:5000/views?page=${page}&&size=${size}`
       );
       const data = await response.json();
       setHistories(data);
@@ -23,30 +22,40 @@ const HistoryList = () => {
 
       setLoading(false);
     });
-  }, []);
+  }, [page]);
 
   console.log(histories);
-  const handleSearch = e => {
-    const searchTextValue = e.target.value;
-    const UserMatch = histories.filter(history => history?.title?.toLowerCase().includes(searchTextValue.toLowerCase()));
-    setsearchHistories(UserMatch);
-  }
 
-  const userHistory = searchHistories.filter(h => h?.viewerEmail === user?.email)
+  const handleSearch = (e) => {
+    const searchTextValue = e.target.value;
+    const UserMatch = histories.filter((history) =>
+      history?.title?.toLowerCase().includes(searchTextValue.toLowerCase())
+    );
+    setsearchHistories(UserMatch);
+  };
+
+  const userHistory = searchHistories.filter(
+    (h) => h?.viewerEmail === user?.email
+  );
 
   return (
     <div style={{ color: "white" }}>
       <Box>
         <div class="wrap">
           <div class="search">
-            <input type="text" class="searchTerm" placeholder="What are you looking for?" onChange={handleSearch} />
+            <input
+              type="text"
+              class="searchTerm"
+              placeholder="What are you looking for?"
+              onChange={handleSearch}
+            />
             <button type="submit" class="searchButton">
               <i class="fa fa-search"></i>
             </button>
           </div>
         </div>
         {!loading ? (
-          userHistory?.map((history) => (
+          histories?.map((history) => (
             <HistoryVideo history={history}></HistoryVideo>
           ))
         ) : (
