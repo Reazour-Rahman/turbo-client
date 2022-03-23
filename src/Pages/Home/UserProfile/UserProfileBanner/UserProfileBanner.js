23c19bd41590ffa8c3b8c4f74c9c0a3d45bfebe1
Reazour Rahaman Rabbi <alexrabbi111@gmail.com>
2022-03-23 21:39:13 +0600

    gg

--- src/Pages/Home/UserProfile/UserProfileBanner/UserProfileBanner.js
+++ src/Pages/Home/UserProfile/UserProfileBanner/UserProfileBanner.js
@@ -2,16 +2,19 @@ import { Box, Grid } from "@mui/material";
 import React from "react";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import { useLocation, useParams } from "react-router-dom";
 
 const UserProfileBanner = () => {
   const [cover, setCover] = useState('')
-  const user = useSelector(state => state.firebase.user)
+  const user = useSelector(state => state.firebase.user);
+  const {email} = useParams();
+
+  console.log(email);
   useEffect(() => {
-    fetch(`https://aqueous-chamber-45567.herokuapp.com/users/room/${user?.email}`)
+    fetch(`https://aqueous-chamber-45567.herokuapp.com/users/room/${email}`)
     .then(res => res.json())
     .then(data => setCover(data.room.cover))
-  },[user?.email])
-  console.log(cover);
+  },[email])
 
   let theme;
   theme = localStorage.getItem("theme");
@@ -22,7 +25,7 @@ const UserProfileBanner = () => {
   return (
     <div>
       <Grid>
-                <Box
+          <Box
           style={{ 
             width: "100%",
             height: "400px", 
