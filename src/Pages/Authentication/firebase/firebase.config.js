545eb3d107e1b2516542acfc07e9633c2d06f6ae
Reazour Rahaman Rabbi <86196606+Reazour-Rahman@users.noreply.github.com>
2023-03-11 19:09:44 +0600

    Update firebase.config.js

--- src/Pages/Authentication/firebase/firebase.config.js
+++ src/Pages/Authentication/firebase/firebase.config.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyD-czdmWW0zmWS8YQ5-pKfjKBeHw9LGKww",
-  authDomain: "turbo-dynamics-pro.firebaseapp.com",
-  projectId: "turbo-dynamics-pro",
-  storageBucket: "turbo-dynamics-pro.appspot.com",
-  messagingSenderId: "472959875726",
-  appId: "1:472959875726:web:b3ba56785e0235f4f667df"
+  apiKey: "AIzaSyD3v21lvTPNKOfdHfzmShC0XUw_DJ6Mr_A",
+  authDomain: "pro-player-26a1c.firebaseapp.com",
+  projectId: "pro-player-26a1c",
+  storageBucket: "pro-player-26a1c.appspot.com",
+  messagingSenderId: "129536068816",
+  appId: "1:129536068816:web:c8d326d8c6af8be76ee2d5"
 };
 export default firebaseConfig;
